f110457db3b5b96c3aa0e7fb617c04450eb3125c
Matt Gray <matthew.thomas.gray@gmail.com>
2011-07-25 18:46:39 +0100

    parse strings atoms and bools

--- scheme.hs
+++ scheme.hs
@@ -2,10 +2,14 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
+-- main
+
 main :: IO ()
 main = do 
     args <- getArgs
     putStrLn $ readExpr $ head args
+
+-- parser
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -17,3 +21,35 @@ readExpr input = case parse (spaces >> symbol) "lisp" input of
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+--list atom
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many $ letter <|> digit <|> symbol
+    let atom = first:rest
+    return $ case atom of
+        "#t" -> Bool True
+        "#f" -> Bool False
+        _    -> Atom atom   
+
+-- lisp string
+
+parseString :: Parser LispVal
+parseString = do 
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
+
+-- language
+
+data LispVal    = Atom String
+                | List [LispVal]
+                | DottedList [LispVal] LispVal
+                | Number Integer
+                | String String
+                | Bool Bool
+
+
