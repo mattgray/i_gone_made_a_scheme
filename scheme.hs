d13f9fccf0cbb9a2345828469fa3129abd4e2eba
Matt Gray <matthew.thomas.gray@gmail.com>
2011-07-26 19:24:19 +0100

    added escaped characters, parsing for deciml and binary prefixes

--- scheme.hs
+++ scheme.hs
@@ -13,7 +13,7 @@ main = do
 -- parser
 
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
@@ -23,30 +23,62 @@ readExpr input = case parse parseExpr "lisp" input of
 spaces :: Parser ()
 spaces = skipMany1 space
 
---list atom
+--lisp atom
 
 parseAtom :: Parser LispVal
 parseAtom = do
     first <- letter <|> symbol
     rest <- many $ letter <|> digit <|> symbol
     let atom = first:rest
-    return $ case atom of
-        "#t" -> Bool True
-        "#f" -> Bool False
-        _    -> Atom atom   
+    return $ Atom atom   
 
 -- lisp string
 
 parseString :: Parser LispVal
 parseString = do 
     char '"'
-    x <- many (noneOf "\"")
+    x <- many $ unescaped <|> escaped
     char '"'
     return $ String x
+    where
+        unescaped = noneOf "\\\""
+        escaped =  do 
+            char '\\'
+            x <- oneOf "\\\"nrt"
+            return $ case x of
+                '\\' -> x
+                '\"' -> x
+                'n' -> '\n'
+                'r' -> '\r'
+                't' -> '\t'
+
+--lisp bool
+parseBool :: Parser LispVal
+parseBool = do
+    char '#'
+    x <- oneOf "tf"
+    return $ case x of
+        't' -> Bool True
+        'f' -> Bool False
+
 
 -- lisp number
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = parsePlainDigits <|> parsePrefixedDigits <|> parseBinary 
+
+digits = many1 digit
+
+parsePlainDigits = liftM (Number . read) $ digits
+
+parsePrefixedDigits = liftM (Number . read) $ try $ string "#d" >> digits
+
+parseBinary = liftM (Number . readBinary) $ try $ string "#b" >> (many1 $ oneOf "01")
+
+readBinary :: String -> Integer
+readBinary = foldl readBinary' 0 where 
+    readBinary' acc '0' = acc * 2  
+    readBinary' acc '1' = (acc * 2) + 1  
+
 
 -- expression
 
@@ -54,6 +86,7 @@ parseExpr :: Parser LispVal
 parseExpr = parseAtom 
     <|> parseString
     <|> parseNumber
+    <|> parseBool
 
 -- language
 
